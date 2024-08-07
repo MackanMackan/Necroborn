a9342b4891091176a5fdd2f661ca249d62f23db8
MackanMackan <38558616+MackanMackan@users.noreply.github.com>
2024-08-07 19:52:50 +0200

    Add properties for Item SO

--- Assets/_Scripts/Inventory/Items/ItemBase.cs
+++ Assets/_Scripts/Inventory/Items/ItemBase.cs
@@ -6,4 +6,8 @@ public class ItemBase : ScriptableObject
     [SerializeField] private string m_itemID;
     [SerializeField] private int m_maxStackSize;
     [SerializeField] private Sprite m_itemIcon;
+    
+    public string ItemID => m_itemID;
+    public int MaxStackSize => m_maxStackSize;
+    public Sprite ItemIcon => m_itemIcon;
 }
