e6d47cfcea3d8363a8301fd4f4fdc56da355ad69
MackanMackan <MackanMackan@users.noreply.github.com>
2025-03-18 17:18:29 +0100

    Added particles to attackables taking damage

--- Assets/_Scripts/Enemy/AttackableTarget.cs
+++ Assets/_Scripts/Enemy/AttackableTarget.cs
@@ -5,13 +5,16 @@ namespace _Scripts.Enemy
     public class AttackableTarget : MonoBehaviour
     {
         [SerializeField] private int m_health;
+        [SerializeField] private ParticleSystem m_damagedParticles;
 
         public void Attack(int damage)
         {
             m_health -= damage;
-            
             if(m_health < 0)
                 DoDeath();
+            
+            if(m_damagedParticles == null) return;
+            m_damagedParticles.Play();
         }
 
         private void DoDeath()
