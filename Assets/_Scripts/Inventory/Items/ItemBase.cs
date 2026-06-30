3f7ef1d5edfeef506ff0670928a9f55a69eb8c99
MackanMackan <MackanMackan@users.noreply.github.com>
2026-06-30 07:29:57 +0200

    add value and tool tips for items

--- Assets/_Scripts/Inventory/Items/ItemBase.cs
+++ Assets/_Scripts/Inventory/Items/ItemBase.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObjects/Item")]
 public class ItemBase : ScriptableObject
 {
+    [SerializeField] private Sprite m_itemIcon;
     [SerializeField] private string m_itemID;
     [SerializeField] private int m_maxStackSize;
-    [SerializeField] private Sprite m_itemIcon;
+    [SerializeField] private int m_value;
+    [SerializeField, TextArea] private string m_toolTip;
     
+    public Sprite ItemIcon => m_itemIcon;
     public string ItemID => m_itemID;
     public int MaxStackSize => m_maxStackSize;
-    public Sprite ItemIcon => m_itemIcon;
+    public int Value => m_value;
+    public string ToolTip => m_toolTip;
 }
